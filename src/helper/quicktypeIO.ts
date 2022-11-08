d9d701408d766cf0aa5fa81135c1325132c6596b
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2022-11-08 12:10:27 +0100

    simplify quicktype i.o conversion

---
+++ src/helper/quicktypeIO.ts
@@ -0,0 +1,20 @@
+import {
+    TargetLanguage, Options
+} from "quicktype-core";
+import fs from "fs";
+import quicktypeJSON from "./quicktype";
+
+export async function quicktypeFromFile(targetLanguage: string | TargetLanguage, typeName: string, options: Partial<Options & {fileExtension: string, inputFile: string, outputFile: string}> = {}) {
+    const {
+        fileExtension = 'js',
+        inputFile = `data/${typeName}_data_pretty.json`,
+        outputFile = `./data/${targetLanguage}/${typeName}.${fileExtension}` } = options;
+    const jsonString = fs.readFileSync(inputFile)
+    const { lines } = await quicktypeJSON(
+        targetLanguage, typeName,
+        jsonString.toString(),
+        options
+    );
+    fs.writeFileSync(outputFile, lines.join('\n'))
+}
+
