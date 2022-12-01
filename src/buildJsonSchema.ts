4545556f9d2636ad8251219149eb2d468356f675
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2022-12-01 10:17:14 +0100

    add an $id in postprocessing stepp when generating schema

--- src/buildJsonSchema.ts
+++ src/buildJsonSchema.ts
@@ -2,6 +2,12 @@ import {INDICES} from "./context";
 import {quicktypeFromFile} from "./helper/quicktypeIO";
 
 const buildJsonSchema = async () => INDICES.map(async (typeName: string) =>
-    await quicktypeFromFile('schema', typeName, {fileExtension: 'json'}));
+    await quicktypeFromFile('schema', typeName, {
+        fileExtension: 'json',
+        postProcess: (generated, outFilename) => {
+            const json = JSON.parse(generated);
+            json.$id = `./${outFilename}`;
+            return JSON.stringify(json, null, 2);
+        }}));
 
 buildJsonSchema();
