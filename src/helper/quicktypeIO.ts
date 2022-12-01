22cc862b37913c075764d50a54995ed3592495cf
Sebastian Tilsch <sebastian.tilsch@slub-dresden.de>
2022-12-01 10:20:48 +0100

    more options (outFilename)

--- src/helper/quicktypeIO.ts
+++ src/helper/quicktypeIO.ts
@@ -1,21 +1,26 @@
 import fs from "fs";
 import {
-Options,
-    TargetLanguage} from "quicktype-core";
+    Options,
+    TargetLanguage
+} from "quicktype-core";
 
 import quicktypeJSON from "./quicktype";
 
-export async function quicktypeFromFile(targetLanguage: string | TargetLanguage, typeName: string, options: Partial<Options & {fileExtension: string, inputFile: string, outputFile: string}> = {}) {
+export async function quicktypeFromFile(targetLanguage: string | TargetLanguage, typeName: string, options: Partial<Options & {outFilename?: string, fileExtension: string, inputFile: string, outputFile: string, postProcess?: (generated: string, outFilename: string) => string}> = {}) {
     const {
         fileExtension = 'js',
         inputFile = `data/${typeName}_data_pretty.json`,
-        outputFile = `./data/${targetLanguage}/${typeName}.${fileExtension}` } = options;
+        outFilename = `${typeName}.${fileExtension}`,
+        outputFile = `./data/${targetLanguage}/${outFilename}`,
+        postProcess
+    } = options;
     const jsonString = fs.readFileSync(inputFile)
     const { lines } = await quicktypeJSON(
         targetLanguage, typeName,
         jsonString.toString(),
         options
     );
-    fs.writeFileSync(outputFile, lines.join('\n'))
+    const generated = lines.join("\n");
+    fs.writeFileSync(outputFile, postProcess ? postProcess(generated, outFilename) : generated);
 }
 
